2941b9ec88007b17508ba56a4febecdd2f0af595
Crevan <a.siverskii@gmail.com>
2023-01-11 19:15:58 +0100

    [add] service interface realisation

---
+++ src/main/java/com/crevan/librarymanager/service/BookServiceImpl.java
@@ -0,0 +1,48 @@
+package com.crevan.librarymanager.service;
+
+import com.crevan.librarymanager.dao.BookDao;
+import com.crevan.librarymanager.model.Book;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private BookDao bookDao;
+
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    @Override
+    @Transactional
+    public void addBook(Book book) {
+        bookDao.addBook(book);
+    }
+
+    @Override
+    @Transactional
+    public void deleteBook(int id) {
+        bookDao.deleteBook(id);
+    }
+
+    @Override
+    @Transactional
+    public void updateBook(Book book) {
+        bookDao.updateBook(book);
+    }
+
+    @Override
+    @Transactional
+    public Book getBookById(int id) {
+        return bookDao.getBookById(id);
+    }
+
+    @Override
+    @Transactional
+    public List<Book> getAllBooks() {
+        return bookDao.getAllBooks()  ;
+    }
+}
